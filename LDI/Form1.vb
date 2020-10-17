5ec3257b0d468b63cb61892962cefa44c1273b84
leiqunni <leiqunni@gmail.com>
2020-10-17 21:57:23 +0900

    build with Visual Studio 2019
    version 2020.10.17.0

---
+++ LDI/Form1.vb
@@ -0,0 +1,229 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Private IndicatorList As New List(Of CIndicator)
+    Private SkinFile As String = "Skin.ini"
+    Private LangFile As String = "Lang.ini"
+
+#Region "Form"
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Me.Load
+        InitLang()
+        InitSkinCombobox()
+        InitDriveListView()
+        InitIndicator()
+
+        If My.Settings.Minimized Then
+            Me.WindowState = FormWindowState.Minimized
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As System.Object, e As System.Windows.Forms.FormClosedEventArgs) Handles MyBase.FormClosed
+        For Each item As CIndicator In IndicatorList
+            item.Dispose()
+        Next
+    End Sub
+
+    Private Sub Form1_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized And IndicatorList.Count > 0 Then
+            Me.ShowInTaskbar = False
+            Me.FormBorderStyle = Windows.Forms.FormBorderStyle.FixedToolWindow
+        Else
+            Me.ShowInTaskbar = True
+            Me.FormBorderStyle = Windows.Forms.FormBorderStyle.Sizable
+        End If
+    End Sub
+
+#End Region
+
+#Region "InitSkinCombobox"
+
+    Private Sub InitSkinCombobox()
+        cmbSkin.Items.Clear()
+        cmbSkin.Items.Add("Default")
+        Dim ini As New IniFile11(SkinFile)
+        For Each value As String In ini.ReadKeyRegex("^\[(.+?)\]", 1)
+            cmbSkin.Items.Add(value)
+        Next
+        If My.Settings.Skin = "" Then
+            cmbSkin.Text = "Default"
+        Else
+            cmbSkin.Text = My.Settings.Skin
+        End If
+    End Sub
+
+#End Region
+
+#Region "InitDriveListView"
+
+    Private Sub InitDriveListView()
+        lsvDrive.Items.Clear()
+        For Each di As DriveInfo In DriveInfo.GetDrives()
+            If di.IsReady And di.DriveType = DriveType.Fixed Then
+                Dim item As New ListViewItem(di.Name.TrimEnd("\\") & " " & If(di.VolumeLabel <> "", di.VolumeLabel, "Local Disk"))
+                item.SubItems.AddRange({"", ""})
+                item.Name = di.Name.TrimEnd({":"c, "\"c})
+
+                If My.Settings.Drives.Contains(di.Name.TrimEnd("\\")) Then
+                    item.Checked = True
+                End If
+
+                item.Tag = di.Name.TrimEnd("\\")
+                lsvDrive.Items.Add(item)
+            End If
+        Next
+    End Sub
+
+#End Region
+
+#Region "InitIndicator"
+
+    Private Sub InitIndicator()
+        For Each item As CIndicator In IndicatorList
+            item.Dispose()
+        Next
+
+        IndicatorList.Clear()
+
+        Dim d As New List(Of String)
+
+        Try
+            For Each item As String In My.Settings.Drives.Trim.Split(" ")
+                Dim di = New IO.DriveInfo(item)
+                If di.IsReady Then
+                    d.Add(item)
+                End If
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return
+        End Try
+
+        '// load skin.
+        Dim _skin = New CSkin()
+        _skin.Load(SkinFile, My.Settings.Skin)
+
+        If _skin.Wide Then
+            For i As Integer = 0 To d.Count - 1
+                IndicatorList.Add(New CIndicator({d(i)}, _skin))
+            Next
+        Else
+            For i As Integer = 0 To d.Count - If(d.Count Mod 2 <> 0, 2, 1) Step 2
+                IndicatorList.Add(New CIndicator({d(i), d(i + 1)}, _skin))
+            Next
+            If d.Count Mod 2 <> 0 Then
+                IndicatorList.Add(New CIndicator({d(d.Count - 1)}, _skin))
+            End If
+        End If
+    End Sub
+
+    Private elapsed As Integer = 0
+
+    Private Sub tmrGlobal_Tick(sender As Object, e As EventArgs) Handles tmrGlobal.Tick
+        If My.Settings.RestartEveryHour Then
+            elapsed += 1
+            If elapsed > 3600 Then
+                elapsed = 0
+                InitIndicator()
+            End If
+        End If
+    End Sub
+
+#End Region
+
+#Region "ListView Mouse Sorting"
+
+    Private Sub lsvDrive_ItemDrag(ByVal sender As System.Object, ByVal e As System.Windows.Forms.ItemDragEventArgs) Handles lsvDrive.ItemDrag
+        DoDragDrop(e.Item, DragDropEffects.Move)
+    End Sub
+
+    Private Sub lsvDrive_DragEnter(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles lsvDrive.DragEnter
+        e.Effect = DragDropEffects.Move
+    End Sub
+
+    Private Sub lsvDrive_DragDrop(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles lsvDrive.DragDrop
+        Dim lvItem As ListViewItem
+        Dim destItem As ListViewItem
+        Dim destLv As ListView = CType(sender, ListView)
+        Dim clX As Integer = destLv.PointToClient(New Point(e.X, e.Y)).X
+        Dim clY As Integer = destLv.PointToClient(New Point(e.X, e.Y)).Y
+        If e.Data.GetDataPresent("System.Windows.Forms.ListViewItem", False) Then
+            'dragging a listview item
+            lvItem = CType(e.Data.GetData("System.Windows.Forms.ListViewItem"), ListViewItem)
+            destItem = CType(sender, ListView).GetItemAt(clX, clY)
+            destLv.Items.Insert(destItem.Index, lvItem.Clone)
+            lvItem.Remove()
+        End If
+    End Sub
+
+#End Region
+
+#Region "ContextMenu"
+
+    Public Sub cmsRestore_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmsRestore.Click
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    Private Sub cmsExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmsExit.Click
+        End
+    End Sub
+
+#End Region
+
+#Region "Form Tools"
+
+    Private Sub btnApply_Click(sender As System.Object, e As System.EventArgs) Handles btnApply.Click
+        My.Settings.Skin = cmbSkin.Text
+        Dim d As String = ""
+        For Each item As ListViewItem In lsvDrive.Items
+            If item.Checked Then d = d & item.Tag & " "
+        Next
+        My.Settings.Drives = d
+        My.Settings.Save()
+        InitIndicator()
+    End Sub
+
+    Private Sub btnReload_Click(sender As Object, e As EventArgs) Handles btnReload.Click
+        InitDriveListView()
+    End Sub
+
+
+#End Region
+
+#Region "InitLang"
+
+    Private Sub InitLang()
+        If IO.File.Exists(LangFile) Then
+            Dim ini = New IniFile11(LangFile)
+            TabControl1.TabPages(0).Text = ini.ReadString("[Lang]Drives")
+            TabControl1.TabPages(1).Text = ini.ReadString("[Lang]Options")
+            TabControl1.TabPages(2).Text = ini.ReadString("[Lang]About")
+            btnReload.Text = ini.ReadString("[Lang]Reload")
+            btnApply.Text = ini.ReadString("[Lang]Apply")
+            lblSkin.Text = ini.ReadString("[Lang]Skin")
+            chbMinimized.Text = ini.ReadString("[Lang]Minimized")
+            chbToolTip.Text = ini.ReadString("[Lang]Tooltip")
+            chbUseScrlLockLed.Text = ini.ReadString("[Lang]UseScrlLockLed")
+            chbRestartEveryHour.Text = ini.ReadString("[Lang]RestartEveryHour")
+            lblInterval.Text = ini.ReadString("[Lang]Interval")
+            cmsRestore.Text = ini.ReadString("[Lang]Restore")
+            cmsExit.Text = ini.ReadString("[Lang]Exit")
+        End If
+    End Sub
+
+#End Region
+
+#Region "About Text"
+
+    Private Sub TabControl1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles TabControl1.SelectedIndexChanged
+        txtAbout.Text = txtAbout.Text.Replace("%version%", My.Application.Info.Version.ToString)
+    End Sub
+
+
+
+#End Region
+
+End Class
---
+++ LDI/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("LDI Logical Disk Indicator")>
+<Assembly: AssemblyDescription("Logical Disk Indicator")>
+<Assembly: AssemblyCompany("DarkEra Software")>
+<Assembly: AssemblyProduct("LDI Logical Disk Indicator")>
+<Assembly: AssemblyCopyright("Copyright (c) 793 ◆7BJkZFw08A / DarkEra Software 2012-2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("9d7d1232-aec3-454b-b8cf-c92f501cfb06")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      Revision
+'
+' すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("2020.10.17.0")>
+<Assembly: AssemblyFileVersion("2020.10.17.0")>
